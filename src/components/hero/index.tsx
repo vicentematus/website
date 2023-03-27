899a07e046da128c503d08890766aa890c30746a
vicentematus <vicentematusortiz@gmail.com>
2023-03-26 23:56:16 -0300

    fix: white letters not working correctly

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -24,10 +24,10 @@ const Hero = () => {
                   alt="me in 35 mm boy"
                 />
               </div>
-              <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+              <h1 className="text-4xl font-bold tracking-tight text-zinc-100 sm:text-5xl">
                 {t("hero.title")}
               </h1>
-              <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+              <p className="mt-6 text-base text-zinc-400 ">
                 {t("hero.description")}
               </p>
               <div className="mt-6 flex gap-6">
